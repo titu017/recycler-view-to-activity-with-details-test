fcbae5279a2e42ac008b2dca0ce8667110c6c277
Titu <titu017@gmail.com>
2019-04-15 23:30:31 +0600

    intent data branch

--- app/src/main/java/com/softgather/rcyclerviewtoactivity/mDetail/ActivityDetails.java
+++ app/src/main/java/com/softgather/rcyclerviewtoactivity/mDetail/ActivityDetails.java
@@ -1,15 +1,36 @@
 package com.softgather.rcyclerviewtoactivity.mDetail;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.softgather.rcyclerviewtoactivity.R;
 
 public class ActivityDetails extends AppCompatActivity {
+    TextView playerName;
+    ImageView playerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        //Initialize View
+
+        playerName = findViewById(R.id.playerNameTv);
+        playerImage = findViewById(R.id.playerImageIv);
+
+        //Receiving Data
+
+        Intent intent = this.getIntent();
+        String name = intent.getExtras().getString("NAME_KEY");
+        int image = intent.getExtras().getInt("IMAGE_KEY");
+
+        //Bind Data
+
+        playerName.setText(name);
+        playerImage.setImageResource(image);
     }
 }
