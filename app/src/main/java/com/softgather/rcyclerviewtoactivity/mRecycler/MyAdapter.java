63c0dc6a8746062d8c7eff566ce959ffb78b2fa7
Titu <titu017@gmail.com>
2019-04-15 22:32:02 +0600

    recyclerview adapter task

--- app/src/main/java/com/softgather/rcyclerviewtoactivity/mRecycler/MyAdapter.java
+++ app/src/main/java/com/softgather/rcyclerviewtoactivity/mRecycler/MyAdapter.java
@@ -1,4 +1,52 @@
 package com.softgather.rcyclerviewtoactivity.mRecycler;
 
-public class MyAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.softgather.rcyclerviewtoactivity.R;
+import com.softgather.rcyclerviewtoactivity.mData.BdPlayer;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyHolder> {
+
+    Context context;
+    List<BdPlayer> bdPlayers;
+
+    public MyAdapter(Context context, List<BdPlayer> bdPlayers) {
+        this.context = context;
+        this.bdPlayers = bdPlayers;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.model,null);
+
+        MyHolder myHolder = new MyHolder(view);
+
+        return myHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder myHolder, int position) {
+        String name = bdPlayers.get(position).getPlayerName();
+        int image = bdPlayers.get(position).getPlayerImage();
+
+        //Bind Data
+
+        myHolder.playerName.setText(name);
+        myHolder.playerImage.setImageResource(image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return bdPlayers.size();
+    }
 }
--- app/src/main/java/com/softgather/rcyclerviewtoactivity/mRecycler/MyHolder.java
+++ app/src/main/java/com/softgather/rcyclerviewtoactivity/mRecycler/MyHolder.java
@@ -1,4 +1,22 @@
 package com.softgather.rcyclerviewtoactivity.mRecycler;
 
-public class MyHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.softgather.rcyclerviewtoactivity.R;
+
+public class MyHolder extends ViewHolder {
+
+    TextView playerName;
+    ImageView playerImage;
+    public MyHolder(@NonNull View itemView) {
+        super(itemView);
+
+        playerName = itemView.findViewById(R.id.textTv);
+        playerImage = itemView.findViewById(R.id.imageIv);
+    }
 }
