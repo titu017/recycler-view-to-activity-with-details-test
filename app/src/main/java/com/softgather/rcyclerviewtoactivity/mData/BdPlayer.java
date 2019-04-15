d30fc66b5f502f1594473b6f42c82ee75759f0fd
Titu <titu017@gmail.com>
2019-04-15 22:05:13 +0600

    pojo class complete in branch

--- app/src/main/java/com/softgather/rcyclerviewtoactivity/mData/BdPlayer.java
+++ app/src/main/java/com/softgather/rcyclerviewtoactivity/mData/BdPlayer.java
@@ -1,4 +1,25 @@
 package com.softgather.rcyclerviewtoactivity.mData;
 
 public class BdPlayer {
+    String playerName;
+    int playerImage;
+
+    public BdPlayer() {
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getPlayerImage() {
+        return playerImage;
+    }
+
+    public void setPlayerImage(int playerImage) {
+        this.playerImage = playerImage;
+    }
 }
--- app/src/main/java/com/softgather/rcyclerviewtoactivity/mData/BdPlayerCollection.java
+++ app/src/main/java/com/softgather/rcyclerviewtoactivity/mData/BdPlayerCollection.java
@@ -1,4 +1,49 @@
 package com.softgather.rcyclerviewtoactivity.mData;
 
+import com.softgather.rcyclerviewtoactivity.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BdPlayerCollection {
+    public static List<BdPlayer> getBdPlayers()
+    {
+        List<BdPlayer> bdPlayers = new ArrayList<>();
+        BdPlayer bdplayer = null;
+
+        //Add data to collection
+
+        bdplayer = new BdPlayer();
+        bdplayer.setPlayerName("Mashrafe");
+        bdplayer.setPlayerImage(R.drawable.masrafe);
+        bdPlayers.add(bdplayer);
+
+        bdplayer = new BdPlayer();
+        bdplayer.setPlayerName("Mashrafe");
+        bdplayer.setPlayerImage(R.drawable.masrafe);
+        bdPlayers.add(bdplayer);
+
+        bdplayer = new BdPlayer();
+        bdplayer.setPlayerName("Mashrafe");
+        bdplayer.setPlayerImage(R.drawable.masrafe);
+        bdPlayers.add(bdplayer);
+
+        bdplayer = new BdPlayer();
+        bdplayer.setPlayerName("Mashrafe");
+        bdplayer.setPlayerImage(R.drawable.masrafe);
+        bdPlayers.add(bdplayer);
+
+        bdplayer = new BdPlayer();
+        bdplayer.setPlayerName("Mashrafe");
+        bdplayer.setPlayerImage(R.drawable.masrafe);
+        bdPlayers.add(bdplayer);
+
+        bdplayer = new BdPlayer();
+        bdplayer.setPlayerName("Mashrafe");
+        bdplayer.setPlayerImage(R.drawable.masrafe);
+        bdPlayers.add(bdplayer);
+
+        return bdPlayers;
+
+    }
 }
